3d65f2e3201a25f1afd75013dddcbf25bce562b6
Insality <insality@gmail.com>
2014-11-08 00:39:37 +0600

    Changed bird movement

--- Cattus/Entities/Enemy/Bird.cs
+++ Cattus/Entities/Enemy/Bird.cs
@@ -4,7 +4,11 @@ using CocosSharp;
 
 namespace Cattus.Entities.Enemy {
     internal class Bird: Enemy {
-        private const int speed = 150;
+        private const int HSpeed = 150;
+        private int _vSpeed = 0;
+        private const int Amplitude = 900;
+        private const int Velocity = 30;
+        private int dir = -1; // 1- вверх, -1 вниз
 
         public Bird(CCPoint startPos, GameLayer gl): base(startPos, Resources.Bird, gl) {
         }
@@ -14,10 +18,23 @@ namespace Cattus.Entities.Enemy {
                 PositionX = -32;
                 _gameLayer.Score += 1;
             }
-            PositionX += speed*dt;
+            PositionX += HSpeed*dt;
 
-            PositionY = (float) ((5/12f)*Window.WindowSizeInPixels.Height +
-                                 (3/8f)*Math.Sin(PositionX/Settings.ScreenWidth*12f)*Settings.ScreenHeight);
+            _vSpeed += Velocity*dir;
+            if (_vSpeed > Amplitude){
+                _vSpeed = Amplitude;
+                dir = -1;
+            }
+            if (_vSpeed < -Amplitude){
+                _vSpeed = -Amplitude;
+                dir = 1;
+            }
+
+            PositionY += _vSpeed * dt;
+
+
+            //            PositionY = (float) ((5/12f)*Window.WindowSizeInPixels.Height +
+            //                                 (3/8f)*Math.Sin(PositionX/Settings.ScreenWidth*12f)*Settings.ScreenHeight);
         }
     }
 }
